2ee5eff57a4caf2a7e1a0e18169f4b8b72dc98f7
kotobukid <kotobuki@daqua.jp>
2022-07-18 14:47:13 +0900

    ループにkey

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import './App.css';
 import {Clicker} from "./Clicker";
 
 function App() {
-    const [nums, setNums] = useState([] as number[]);
+    const [nums, setNums] = useState([] as {index: number, value: number}[]);
+    const [index, setIndex] = useState(0);
+
     const all_reset = () => {
-        debugger;
-        setNums(nums.map(() => {return 0}))
+        setNums(nums.map((n) => {return {index: n.index, value: 0}}))
     }
 
     return (
@@ -14,14 +15,15 @@ function App() {
             <header className="App-header">
                 hello world
                 <button onClick={() => {
-                    setNums([...nums, 0])
+                    setIndex(index + 1)
+                    setNums([...nums, {index, value: 0}])
                 }}> plus
                 </button>
                 <button onClick={all_reset}>All reset</button>
                 <br/>
                 <div className="buttons">
-                    {nums.map((n: number) => {
-                        return (<Clicker num={n}/>)
+                    {nums.map((n: {index: number, value: number}) => {
+                        return (<Clicker key={n.index} num={n.value}/>)
                     })}
                 </div>
             </header>
